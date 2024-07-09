35ade8a460ea8ab4b8ca999c34be5ecae25573c6
Daniyarsick <dan.annanurov@gmail.com>
2024-07-09 21:55:47 +0300

    Авторизация

---
+++ app/src/main/java/com/example/kotlinsqlite/AuthActivity.kt
@@ -0,0 +1,54 @@
+package com.example.kotlinsqlite
+
+import android.content.Intent
+import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+
+class AuthActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_auth)
+
+        val userLogin: EditText = findViewById(R.id.user_login_auth)
+        val userPass: EditText = findViewById(R.id.user_pass_auth)
+        val button: Button = findViewById(R.id.button_auth)
+        val linktoReg: TextView = findViewById(R.id.link_to_reg)
+
+
+
+        linktoReg.setOnClickListener {
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+        }
+
+        button.setOnClickListener {
+            val login = userLogin.text.toString().trim()
+
+            val pass = userPass.text.toString().trim()
+
+            if (login == "" || pass == "") Toast.makeText(
+                this, "Не все поля заполнены", Toast.LENGTH_LONG
+            ).show()
+            else {
+
+                val db = DbHelper(this, null)
+                var isAuth = db.getUser(login, pass)
+
+                if (isAuth) {
+                    Toast.makeText(this, "Пользователь $login авторизован", Toast.LENGTH_LONG)
+                        .show()
+                    userLogin.text.clear()
+                    userPass.text.clear()
+
+                } else {
+                    Toast.makeText(this, "Пользователь $login НЕ авторизован", Toast.LENGTH_LONG)
+                        .show()
+                }
+            }
+        }
+    }
+}
--- app/src/main/java/com/example/kotlinsqlite/MainActivity.kt
+++ app/src/main/java/com/example/kotlinsqlite/MainActivity.kt
@@ -1,8 +1,10 @@
 package com.example.kotlinsqlite
 
+import android.content.Intent
 import android.os.Bundle
 import android.widget.Button
 import android.widget.EditText
+import android.widget.TextView
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 
@@ -15,18 +17,27 @@ class MainActivity : AppCompatActivity() {
         val userEmail: EditText = findViewById(R.id.user_email)
         val userPass: EditText = findViewById(R.id.user_pass)
         val button: Button = findViewById(R.id.button_reg)
+        val linktoAuth: TextView = findViewById(R.id.link_to_auth)
+
+        linktoAuth.setOnClickListener {
+            val intent = Intent(this, AuthActivity::class.java)
+            startActivity(intent)
+        }
 
         button.setOnClickListener {
             val login = userLogin.text.toString().trim()
             val email = userEmail.text.toString().trim()
             val pass = userPass.text.toString().trim()
 
-            if(login=="" || email == "" || pass == "")
-                Toast.makeText(this, "Не все поля заполнены", Toast.LENGTH_LONG).show()
-            else{
+            if (login == "" || email == "" || pass == "") Toast.makeText(
+                this,
+                "Не все поля заполнены",
+                Toast.LENGTH_LONG
+            ).show()
+            else {
                 val user = User(login, email, pass)
 
-                val db =DbHelper(this,null)
+                val db = DbHelper(this, null)
                 db.addUser(user)
                 Toast.makeText(this, "Пользователь $login добавлен", Toast.LENGTH_LONG).show()
 
