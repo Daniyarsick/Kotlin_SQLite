a0364573db06c88833b59c3dd38c39d9c252c55f
Daniyarsick <dan.annanurov@gmail.com>
2024-07-10 18:39:46 +0300

    Вывод покупки которые есть

--- app/src/main/java/com/example/kotlinsqlite/ItemsAdapter.kt
+++ app/src/main/java/com/example/kotlinsqlite/ItemsAdapter.kt
@@ -1,9 +1,11 @@
 package com.example.kotlinsqlite
 
 import android.content.Context
+import android.content.Intent
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
 import android.widget.ImageView
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
@@ -16,6 +18,7 @@ class ItemsAdapter(var items: List<Item>, var context: Context) :
         val title: TextView = view.findViewById(R.id.item_list_title)
         val desc: TextView = view.findViewById(R.id.item_list_desc)
         val price: TextView = view.findViewById(R.id.item_list_price)
+        val btn: Button = view.findViewById(R.id.item_list_button)
 
 
     }
@@ -37,5 +40,14 @@ class ItemsAdapter(var items: List<Item>, var context: Context) :
         var imageId =
             context.resources.getIdentifier(items[position].image, "drawable", context.packageName)
         holder.image.setImageResource(imageId)
+
+        holder.btn.setOnClickListener{
+            val intent = Intent(context,ItemActivity::class.java)
+
+            intent.putExtra("itemTitle",items[position].title)
+            intent.putExtra("itemText",items[position].text)
+
+            context.startActivity(intent)
+        }
     }
 }
